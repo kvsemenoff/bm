a4a7a09e40c5d5b9d82ae6d69ec3fd9d61c207db
Almaz Dzhanaliev <aldjanaliev@gmail.com>
2017-03-27 20:24:24 +0600

    did petr.php

--- includes/petr.php
+++ includes/petr.php
@@ -21,7 +21,34 @@
 						</div>
 			    </div>
 			    <div class="inner">
-			    	
+			    	<div class="img_wrap">
+			    		<img src="img/petr.jpg" alt="">
+			    	</div>
+			    	<div class="speaker_info">
+			    		<span>Пётр Осипов</span>
+			    		<i>основатель бизнес молодости</i>
+			    	</div>
+			    	<p class="speaker_text">Я видел тысячи участников МЗС. То, что они знали про МЗС после программы и до — два абсолютно разных знания. Сейчас ты видишь только сайт и это так далеко от ядра. Пообщаться с менеджером — уже шаг ближе к ядру. Сайт никогда не расскажет так, как живой человек. Твоя задача приблизиться к ядру, чтобы понять суть МЗС. Стоит ли это 10 минут общения с менеджером? Да, стоит. Поэтому имеет смысл созвониться.</p>
+						<div class="form_wrap">
+							<form action="#">
+								<div class="clearfix">
+									<div class="input_wrap input_wrap__input">
+										<input class="input input__grey" type="text" value="" placeholder="Имя">
+									</div>
+									<div class="input_wrap input_wrap__button">
+										<a href="#" class="button button__blue">Записаться на МЭС</a>
+									</div>
+								</div>
+								<div class="checkbox checkbox__squre">
+									<input id="ch-1" type="checkbox" checked>
+									<label for="ch-1">Даю согласие на обработку персональных данных и соглашаюсь c условиями договора-оферты, лицензионного договора и политикой конфиденциальности. </label>
+								</div>
+								<div class="checkbox checkbox__squre">
+									<input id="ch-2" type="checkbox" checked>
+									<label for="ch-2">Я согласен на получение рекламной информации от Бизнес Молодости. </label>
+								</div>
+							</form>
+						</div>
 			    </div>
 			  </div>
 			</div>
