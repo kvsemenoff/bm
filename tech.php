78d7955a57eda01ea184cddff00cdbbb2200034d
Almaz Dzhanaliev <aldjanaliev@gmail.com>
2017-03-27 15:06:16 +0600

    added button and hashtag

--- tech.php
+++ tech.php
@@ -1,8 +1,24 @@
 ﻿<?php require_once('includes/header.php'); ?>
 
 <br><br>		
-<a href="#" class="button-1">Заказать звонок</a>	
-<br><br>	
+<a href="#" class="button button--blue">Записаться на МЭС</a>
+<i class="margin-bottom"></i>
+<a href="#" class="button button--big button--blue">Записаться на МЭС</a>
+<i class="margin-bottom"></i>
+<a href="#" class="button button--black">Далее</a>
+<i class="margin-bottom"></i>
+<a href="#" class="button button--grey">Далее</a>	
+<i class="margin-bottom"></i>
+<a href="#" class="button button--main">Стать участником Нового МЗС</a>	
+<br><br>
+<a href="#" class="hashtag hashtag--big">#Жесткий разбор с Петром Осиповыс </a>
+<br><br>
+<a href="#" class="hashtag hashtag--medium">#Про МЗС</a>
+<br><br>
+<a href="#" class="hashtag">#Про МЗС</a>
+<br><br>
+<a href="#" class="hashtag hashtag--sm">#Жесткий разбор с Петром Осиповыс </a>
+<br><br>
 <h1>Пример заголовка</h1>
 <br><br>	
 <form action="#">
