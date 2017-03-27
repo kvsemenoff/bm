52649cc9c737689ca2fae8b8720be7f57a734ca6
Almaz Dzhanaliev <aldjanaliev@gmail.com>
2017-03-27 16:54:37 +0600

    added inputs

--- tech.php
+++ tech.php
@@ -2,35 +2,41 @@
 
 <div class="container">
 	<br><br>		
-	<a href="#" class="button button--blue">Записаться на МЭС</a>
+	<a href="#" class="button button__blue">Записаться на МЭС</a>
 	<i class="margin-bottom"></i>
-	<a href="#" class="button button--big button--blue">Записаться на МЭС</a>
+	<a href="#" class="button button__big button__blue">Записаться на МЭС</a>
 	<i class="margin-bottom"></i>
-	<a href="#" class="button button--black">Далее</a>
+	<a href="#" class="button button__black">Далее</a>
 	<i class="margin-bottom"></i>
-	<a href="#" class="button button--grey">Далее</a>	
+	<a href="#" class="button button__grey">Далее</a>	
 	<i class="margin-bottom"></i>
-	<a href="#" class="button button--main">Стать участником Нового МЗС</a>	
+	<a href="#" class="button button__main">Стать участником Нового МЗС</a>	
 	<br><br>
-	<a href="#" class="hashtag hashtag--big">#Жесткий разбор с Петром Осиповыс </a>
+	<a href="#" class="hashtag hashtag__big">#Жесткий разбор с Петром Осиповыс </a>
 	<br><br>
-	<a href="#" class="hashtag hashtag--medium">#Про МЗС</a>
+	<a href="#" class="hashtag hashtag__medium">#Про МЗС</a>
 	<br><br>
 	<a href="#" class="hashtag">#Про МЗС</a>
 	<br><br>
-	<a href="#" class="hashtag hashtag--sm">#Жесткий разбор с Петром Осиповыс </a>
+	<a href="#" class="hashtag hashtag__sm">#Жесткий разбор с Петром Осиповыс </a>
 	<br><br>
 	<h2>Пример заголовка</h2>
 	<br>
 	<form action="#">
-		<input type="text" value="" placeholder="Инпут тайп текст">		
-		<input type="submit" value="Значение" placeholder="Инпут тайп сабмит">
+		<div class="search clearfix">
+			<input type="text" value="" placeholder="Поиск">
+			<button></button>	
+		</div>
+		<br>
+		<input class="input input__grey" type="text" value="" placeholder="Имя">
+		<input class="input input__white" type="text" value="" placeholder="000.000.000">
+		<br><i class="margin-bottom"></i>
 		<br><br>
-		<div class="checkbox checkbox--circle">
+		<div class="checkbox checkbox__circle">
 			<input id="ch-1" type="checkbox" checked>
 			<label for="ch-1">Даю согласие на обработку персональных данных и соглашаюсь c условиями договора-оферты, лицензионного договора и политикой конфиденциальности. </label>
 		</div>
-		<div class="checkbox checkbox--squre">
+		<div class="checkbox checkbox__squre">
 			<input id="ch-2" type="checkbox" checked>
 			<label for="ch-2">Далеко-далеко за словесными горами в стране, гласных и согласных живут рыбные тексты. Даже, образ вопрос своих необходимыми залетают переписали переулка рыбными собрал оксмокс свой грустный это назад подзаголовок текстов ipsum она пунктуация.</label>
 		</div>
