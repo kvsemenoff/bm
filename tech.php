2a7dc691d26fadee0f5473e35cb02edffadab0e7
Almaz Dzhanaliev <aldjanaliev@gmail.com>
2017-03-27 16:01:44 +0600

    added checkbox and h2

--- tech.php
+++ tech.php
@@ -1,30 +1,41 @@
 ﻿<?php require_once('includes/header.php'); ?>
 
-<br><br>		
-<a href="#" class="button button--blue">Записаться на МЭС</a>
-<i class="margin-bottom"></i>
-<a href="#" class="button button--big button--blue">Записаться на МЭС</a>
-<i class="margin-bottom"></i>
-<a href="#" class="button button--black">Далее</a>
-<i class="margin-bottom"></i>
-<a href="#" class="button button--grey">Далее</a>	
-<i class="margin-bottom"></i>
-<a href="#" class="button button--main">Стать участником Нового МЗС</a>	
-<br><br>
-<a href="#" class="hashtag hashtag--big">#Жесткий разбор с Петром Осиповыс </a>
-<br><br>
-<a href="#" class="hashtag hashtag--medium">#Про МЗС</a>
-<br><br>
-<a href="#" class="hashtag">#Про МЗС</a>
-<br><br>
-<a href="#" class="hashtag hashtag--sm">#Жесткий разбор с Петром Осиповыс </a>
-<br><br>
-<h1>Пример заголовка</h1>
-<br><br>	
-<form action="#">
-	<input type="text" value="" placeholder="Инпут тайп текст">		
-	<input type="submit" value="Значение" placeholder="Инпут тайп сабмит">		
-</form>		
+<div class="container">
+	<br><br>		
+	<a href="#" class="button button--blue">Записаться на МЭС</a>
+	<i class="margin-bottom"></i>
+	<a href="#" class="button button--big button--blue">Записаться на МЭС</a>
+	<i class="margin-bottom"></i>
+	<a href="#" class="button button--black">Далее</a>
+	<i class="margin-bottom"></i>
+	<a href="#" class="button button--grey">Далее</a>	
+	<i class="margin-bottom"></i>
+	<a href="#" class="button button--main">Стать участником Нового МЗС</a>	
+	<br><br>
+	<a href="#" class="hashtag hashtag--big">#Жесткий разбор с Петром Осиповыс </a>
+	<br><br>
+	<a href="#" class="hashtag hashtag--medium">#Про МЗС</a>
+	<br><br>
+	<a href="#" class="hashtag">#Про МЗС</a>
+	<br><br>
+	<a href="#" class="hashtag hashtag--sm">#Жесткий разбор с Петром Осиповыс </a>
+	<br><br>
+	<h2>Пример заголовка</h2>
+	<br>
+	<form action="#">
+		<input type="text" value="" placeholder="Инпут тайп текст">		
+		<input type="submit" value="Значение" placeholder="Инпут тайп сабмит">
+		<br><br>
+		<div class="checkbox checkbox--circle">
+			<input id="ch-1" type="checkbox" checked>
+			<label for="ch-1">Даю согласие на обработку персональных данных и соглашаюсь c условиями договора-оферты, лицензионного договора и политикой конфиденциальности. </label>
+		</div>
+		<div class="checkbox checkbox--squre">
+			<input id="ch-2" type="checkbox" checked>
+			<label for="ch-2">Далеко-далеко за словесными горами в стране, гласных и согласных живут рыбные тексты. Даже, образ вопрос своих необходимыми залетают переписали переулка рыбными собрал оксмокс свой грустный это назад подзаголовок текстов ipsum она пунктуация.</label>
+		</div>
+	</form>		
+</div>
 
 
 <?php require_once('includes/footer.php'); ?>
