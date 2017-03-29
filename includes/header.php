5fff9c9f8b296104b95519af2cdde6fae8f14cfd
engine77 <zaur.1kager@gmail.com>
2017-03-29 13:38:13 +0400

    tech

--- includes/header.php
+++ includes/header.php
@@ -10,9 +10,11 @@
 	<link href="//netdna.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.css" rel="stylesheet">
 	<link rel="stylesheet" href="libs/fancybox/jquery.fancybox.css" />
 	<link rel="stylesheet" href="libs/owl.carousel/assets/owl.carousel.css" />
-	<link type="text/css" href="css/jquery.jscrollpane.css" rel="stylesheet" media="all" />
+	<!-- <link type="text/css" href="css/jquery.jscrollpane.css" rel="stylesheet" media="all" /> -->
 	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i" rel="stylesheet">
 	<link rel="stylesheet" href="libs/FormStyle/jquery.formstyler.css">
+	
+	<link rel="stylesheet" href="libs/jquery-mousewheel/jquery.jscrollpane.css">
 
 	<link rel="shortcut icon" href="favicon.ico" />
 	<link rel="stylesheet" href="css/style.css">
@@ -22,10 +24,13 @@
 	<script src="libs/jquery/jquery-1.11.1.min.js"></script>
 	<script src="libs/FormStyle/jquery.formstyler.min.js"></script>
 	<script src="libs/owl.carousel/owl.carousel.js"></script>
-	<script src="libs/slick/slick.min.js"></script>
+	<!-- <script src="libs/slick/slick.min.js"></script> -->
 	<script src="libs/fancybox/jquery.fancybox.pack.js"></script>
 	<script src="libs/bootstrap/bootstrap.min.js"></script>
 	<script src="libs/wow/wow.min.js"></script>
+
+	<script src="libs/jquery-mousewheel/jquery.jscrollpane.js"></script>
+	<script src="libs/jquery-mousewheel/jquery.mousewheel.min.js"></script>
 	<script src="js/jquery.maskedinput.min.js"></script>
 	<script src="js/common.js"></script>
 	
